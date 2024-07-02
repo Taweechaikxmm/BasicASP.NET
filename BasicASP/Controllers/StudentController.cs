b58961921bdb914d5f027cfe47badfc669152787
tawee <tawee@LAPTOP-1U3GAS3H>
2024-07-02 13:08:16 +0700

    Add project files.

---
+++ BasicASP/Controllers/StudentController.cs
@@ -0,0 +1,87 @@
+﻿using BasicASP.Data;
+using BasicASP.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+
+namespace BasicASP.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+
+        public StudentController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable <Student> students = _db.Students;
+            return View(students);
+        }
+
+        public IActionResult Create() {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Student student) {
+            if (ModelState.IsValid) {
+                _db.Students.Add(student);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+
+        //public IActionResult Edit(int? id) {
+        //    var obj = _db.Students.Find(id);
+        //    return View(obj);
+        //}
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Students.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Students.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Students.Remove(obj);
+            _db.SaveChanges(); return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Students.Update(student);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+        public IActionResult About() {
+            return View();
+        }
+    }
+}
